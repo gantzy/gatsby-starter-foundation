11a5a465a14c74317b4f5f402a84f98e7771618a
David Miner <miner49er98@gmail.com>
2021-02-25 22:25:10 -0500

    Logo Updates

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const logoStyle = {
+    width: '100px',
+    float: 'left',
+};
+
+const linkStyle = {
+    float: 'right',
+    height: '50px',
+    lineHeight: '50px',
+};
+
 const Logo = (props) => (
   <div className="site-logo">
-    <img src={props.image} />
-    <Link to="/">{props.title}</Link>
+    <Link to="/">
+        <img src={props.image} style={logoStyle}/>
+        <span style={linkStyle}>{props.title}<span>
+    </Link>
   </div>
-)
+);
 
-export default Logo
+export default Logo;
